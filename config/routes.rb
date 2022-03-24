1c9155fd1170d96f212e3467b048f5fe3567f2c2
Austen Wilde <sawilde15@gmail.com>
2022-03-23 20:17:23 -0600

    users crud done

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
   
   namespace :api do
     resources :users
-    resources :courses 
-    get '/:id/courses', to: 'users#Courses'
-    get '/:id/users', to: 'courses#Users'
+    resources :courses do
+      resources :enrollments
+      get '/:course_id/unenrolled', to: 'enrollments#unenrolledUsers'
+      get '/:course_id/enrolled', to: 'enrollments#enrolledUsers'
+    end
+    get '/:id/courses', to: 'users#courses'
+    get '/:id/users', to: 'courses#users'
   end
 
 end
